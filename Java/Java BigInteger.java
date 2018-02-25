ebbc03a1742eedd0b123ce3512f673ad2e25db99
Uttu316 <uttugupta316@email.com>
2018-02-26 03:25:11 +0530

    Programs
    Java programs

---
+++ Java/Java BigInteger.java
@@ -0,0 +1,21 @@
+/* Utkarsh's Solution*/
+
+import java.io.*; 
+import java.util.*;
+import java.text.*;
+import java.math.*; 
+import java.util.regex.*;
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = new BigInteger(sc.next());
+        BigInteger b = new BigInteger(sc.next());
+        BigInteger sum,mul;
+        sum = a.add(b);
+        mul = a.multiply(b);
+        System.out.println(sum);
+        System.out.println(mul);
+        
+    }
+}
